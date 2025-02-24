50e5047dd0a5151e9c7193cfff140c3d83d0d618
Oleksandr Tukhovskyi <alexx1984@ukr.net>
2025-02-24 12:12:32 +0200

    refactor rate hendler

--- src/Service/ExchangeRateRequestHandler.php
+++ src/Service/ExchangeRateRequestHandler.php
@@ -2,8 +2,6 @@
 
 namespace App\Service;
 
-namespace App\Service;
-
 use App\DTO\PaginationRequest;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -16,31 +14,31 @@ class ExchangeRateRequestHandler
 
     public function handleGetRates(Request $request): array
     {
-        $pagination = new PaginationRequest(
-            $request->query->getInt('page', 1),
-            $request->query->getInt('limit', 10)
+        return $this->processRequest($request, fn($pagination) =>
+        $this->rateService->getRates($pagination->page, $pagination->limit)
         );
-
-        $validated = $this->validator->validate($pagination);
-        if (is_array($validated)) {
-            return ['errors' => $validated];
-        }
-
-        return $this->rateService->getRates($validated->page, $validated->limit);
     }
 
     public function handleGetRate(Request $request, string $name): array
     {
-        $pagination = new PaginationRequest(
+        return $this->processRequest($request, fn($pagination) =>
+        $this->rateService->getRate($name, $pagination->page, $pagination->limit)
+        );
+    }
+
+    private function processRequest(Request $request, callable $callback): array
+    {
+        $pagination = self::createPaginationRequest($request);
+        $validated = $this->validator->validate($pagination);
+
+        return is_array($validated) ? ['errors' => $validated] : $callback($validated);
+    }
+
+    private static function createPaginationRequest(Request $request): PaginationRequest
+    {
+        return new PaginationRequest(
             $request->query->getInt('page', 1),
             $request->query->getInt('limit', 10)
         );
-
-        $validated = $this->validator->validate($pagination);
-        if (is_array($validated)) {
-            return ['errors' => $validated];
-        }
-
-        return $this->rateService->getRate($name, $validated->page, $validated->limit);
     }
 }
